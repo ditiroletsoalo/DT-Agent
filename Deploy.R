a9592d4894a55997894b9311e06a1108191cd374
ditiro letsoalo <dtirow@gmail.com>
2026-03-26 01:02:20 +0200

    new

--- Deploy.R
+++ Deploy.R
@@ -1,6 +1,4 @@
-rsconnect::setAccountInfo(name='ditiroletsoalo',
-                          token='D81FB22AC1E44F8E9542A0D8897A0D91',
-                          secret='/qIciebE1u7nHozjIsGnNkk8mq4lLNWx30OHTRv8')
+
 
 rsconnect::deployApp(
   appName = "ditiro",
--- secrets.R
+++ secrets.R
@@ -1,1 +1,6 @@
 Sys.setenv(MISTRAL_API_KEY = "pS2JsGmuGSPTxBDySiDjuFYwxHlqFuXm")
+
+
+# rsconnect::setAccountInfo(name='ditiroletsoalo',
+#                           token='D81FB22AC1E44F8E9542A0D8897A0D91',
+#                           secret='/qIciebE1u7nHozjIsGnNkk8mq4lLNWx30OHTRv8')
